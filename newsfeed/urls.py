17ef36ae73575b2233fa18dae6e8f261cf36ee96
Sojeb Sikder <sojebsikder@gmail.com>
2021-10-14 21:30:27 +0600

    main

--- newsfeed/urls.py
+++ newsfeed/urls.py
@@ -6,5 +6,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('newsfeed', views.index, name='index')
+    path('newsfeed/', views.newsfeed, name='newsfeed')
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- newsfeed/views.py
+++ newsfeed/views.py
@@ -1,6 +1,11 @@
+import json
+import os
+from django.conf import settings
+from django.http.response import JsonResponse
 import pandas as pd
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.conf.urls.static import static
 
 
 def index(request):
@@ -8,10 +13,19 @@ def index(request):
     return render(request, template_name)
 
 
+def public(url):
+    url = url.replace('/', '\\')
+    path = os.path.join(settings.STATIC_ROOT, url)
+    path = path.replace("\\", '/')
+    return path
+
+
 def newsfeed(request):
-    filepath_dict = {'yelp':   'data/sentiment_analysis/yelp_labelled.txt',
-                    'amazon': 'data/sentiment_analysis/amazon_cells_labelled.txt',
-                    'imdb':   'data/sentiment_analysis/imdb_labelled.txt'}
+    filepath_dict = {
+        'yelp':  public('data/sentiment_analysis/yelp_labelled.txt'),
+        'amazon': public('data/sentiment_analysis/amazon_cells_labelled.txt'),
+        'imdb':   public('data/sentiment_analysis/imdb_labelled.txt'),
+    }
 
     df_list = []
     for source, filepath in filepath_dict.items():
@@ -20,6 +34,6 @@ def newsfeed(request):
         df_list.append(df)
 
     df = pd.concat(df_list)
-    print(df.iloc[0])
+    # print(df.iloc[0])
 
-    return HttpResponse("Hello World")
+    return JsonResponse(json.loads(df.iloc[0].to_json()),safe= False)
